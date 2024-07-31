d6f0d7afc8cdc0e83f00dfda3a683875a6166e5d
Concreate™ <96336564+concreatehq@users.noreply.github.com>
2024-07-31 16:05:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const lastSentTimes = {};
 function generateOTP() {
     const digits = '0123456789';
     let OTP = '';
-    for (let i = 0; i < 6; i++) {
+    for (let i = 0; i < 4; i++) {
         OTP += digits[Math.floor(Math.random() * 10)];
     }
     return OTP;
